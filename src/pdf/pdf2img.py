6ea1319d753a5c15283b8df435c525d8c51fe425
eric-yyjau <eric.yyjau@gmail.com>
2018-12-04 13:09:20 -0800

    finish testing

--- src/pdf/pdf2img.py
+++ src/pdf/pdf2img.py
@@ -17,21 +17,37 @@ class PdfFile:
             img = cv2.cvtColor(np.array(self.pages[pageNum]), cv2.COLOR_RGB2BGR)
             return img
         else:
-            return -1
+            print("out of range")
+            return []
+
 
 
 if __name__ == '__main__':
-    filename = 'pdf/test1.pdf'
+    filename = 'test1.pdf'
     file = PdfFile()
     totalPgs = file.readFile(filename)
     print("total pages = ", totalPgs)
 
-    img = file.readPage(0)
+    pageNum = 0
+    img = file.readPage(pageNum)
+    img_next = []
     height, width = img.shape[:2]
     while True:
         img = cv2.resize(img,(height//10, width//10))
         cv2.imshow('img', img)
         key = cv2.waitKey(1)
+        if key == 97: #'a'
+            pageNum = pageNum-1
+            img_next = file.readPage(pageNum)
+            print ("detect a")
+        elif key == 100: #'d'
+            pageNum = pageNum+1
+            img_next = file.readPage(pageNum)
+            print ("detect d")
+        if img_next != []:
+            img = img_next
+
         if key == 27:
             print("stop")
             break
+        # else if key == 
