af8cca6f174e903b3e4498abf2c9393e4f2c9d31
eric-yyjau <eric.yyjau@gmail.com>
2018-12-06 17:57:03 -0800

    update page flipping

--- src/page_flip.py
+++ src/page_flip.py
@@ -2,71 +2,81 @@ import RPi.GPIO as IO
 from time import sleep
 import time
 
-IO.setmode(IO.BCM)
-IO.setup(12,IO.IN)
-IO.setup(19, IO.IN)
-prev = "temp"
-prev_ts = time.time()
+def ReadSensor():
+    global page_action, detectPage
+    IO.setmode(IO.BCM)
+    IO.setup(12,IO.IN)
+    IO.setup(19, IO.IN)
+    prev = "temp"
+    prev_ts = time.time()
 
-state = "start"
-A_val = "temp"
-A_ts = 0
-B_val = "temp"
-start_ts = 0
-while True:
-    isLeft = IO.input(12)
-    isRight = IO.input(19)
-    if state == "hit_two" or state == "start":
-        if not (isLeft or isRight):
-            continue
-        if (B_val == "left" and isRight) or (B_val == "right" and isLeft) or (A_val == "temp" and B_val == "temp"):
-            state = "hit_one"
+    state = "start"
+    A_val = "temp"
+    A_ts = 0
+    B_val = "temp"
+    start_ts = 0
+    while True:
+        isLeft = IO.input(12)
+        isRight = IO.input(19)
+        if state == "hit_two" or state == "start":
+            if not (isLeft or isRight):
+                continue
+            if (B_val == "left" and isRight) or (B_val == "right" and isLeft) or (A_val == "temp" and B_val == "temp"):
+                state = "hit_one"
+                
+                if isLeft:
+                    A_val = "left"
+                if isRight:
+                    A_val = "right"
+                print ("going to state 1", A_val, B_val)
+                start_ts = time.time()
             
-            if isLeft:
-                A_val = "left"
-            if isRight:
-                A_val = "right"
-            print "going to state 1", A_val, B_val
-            start_ts = time.time()
-        
-        
+            
 
-    if state == "hit_one" :
-        if time.time()-start_ts > 2.0:
-            state = "hit_two"
-            A_val = "temp"
-            B_val = "temp"
-            print "time exceeded", A_val, B_val
-        if not (isLeft or isRight):
-            continue
-        if (A_val == "left" and isRight) or (A_val == "right" and isLeft):
-            
-            state = "hit_two"
-            if isLeft:
-                B_val = "left"
-            if isRight:
-                B_val = "right"
-            if A_val == "left" and B_val == "right":
-                print "front flip", A_val, B_val
-                while True:
-                    if not (IO.input(12) or IO.input(19)):
-                        time.sleep(0.1)
-                        if not (IO.input(12) or IO.input(19)):
-                            break
+        if state == "hit_one" :
+            if time.time()-start_ts > 2.0:
+                state = "hit_two"
                 A_val = "temp"
                 B_val = "temp"
-
-            if A_val == "right" and B_val == "left":
-                print "back flip", A_val, B_val
-                while True:
-                    if not (IO.input(12) or IO.input(19)):
-                        time.sleep(0.1)
+                print ("time exceeded", A_val, B_val)
+            if not (isLeft or isRight):
+                continue
+            if (A_val == "left" and isRight) or (A_val == "right" and isLeft):
+                
+                state = "hit_two"
+                if isLeft:
+                    B_val = "left"
+                if isRight:
+                    B_val = "right"
+                if A_val == "left" and B_val == "right":
+                    print ("front flip", A_val, B_val)
+                    while True:
                         if not (IO.input(12) or IO.input(19)):
-                            break
-                A_val = "temp"
-                B_val = "temp"
-            print "going to state 2", A_val, B_val
-        else:
-            pass
+                            time.sleep(0.1)
+                            if not (IO.input(12) or IO.input(19)):
+                                break
+                    A_val = "temp"
+                    B_val = "temp"
 
-    sleep(0.01)
+                if A_val == "right" and B_val == "left":
+                    print ("back flip", A_val, B_val)
+                    while True:
+                        if not (IO.input(12) or IO.input(19)):
+                            time.sleep(0.1)
+                            if not (IO.input(12) or IO.input(19)):
+                                break
+                    A_val = "temp"
+                    B_val = "temp"
+                print ("going to state 2", A_val, B_val)
+            else:
+                pass
+
+        sleep(0.01)
+
+if __name__=="__main__":
+    page_action = []
+    detectPage = False
+    ReadSensor()
+
+
+
